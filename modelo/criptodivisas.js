436b4520eba7bec6c41f78ced5478ef71a82b451
Jorge <jorgebeybo@hotmail.com>
2021-04-06 02:53:29 +0200

    Eliminado las imágenes del modelo de las divisas y agregado el campo color.

--- modelo/criptodivisas.js
+++ modelo/criptodivisas.js
@@ -2,74 +2,42 @@ export default {
     bitcoin:{
         simbolo: 'btc',
         nombre: 'Bitcoin',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/1/thumb/bitcoin.png?1547033579',
-            peque: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579',
-            grande: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'
-        }
+        color:'#f2a900'
     },
     ethereum:{
         simbolo: 'eth',
         nombre: 'Ethereum',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/279/thumb/ethereum.png?1595348880',
-            peque: 'https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880',
-            grande: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880'
-        }
+        color:'#627eea'
     },
     polkadot:{
         simbolo: 'dot',
         nombre: 'Polkadot',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/12171/thumb/aJGBjJFU_400x400.jpg?1597804776',
-            peque: 'https://assets.coingecko.com/coins/images/12171/small/aJGBjJFU_400x400.jpg?1597804776',
-            grande: 'https://assets.coingecko.com/coins/images/12171/large/aJGBjJFU_400x400.jpg?1597804776'
-        }
+        color:'#e6007a'
     },
     cardano:{
         simbolo: 'ada',
         nombre: 'Cardano',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/975/thumb/cardano.png?1547034860',
-            peque: 'https://assets.coingecko.com/coins/images/975/small/cardano.png?1547034860',
-            grande: 'https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860'
-        }
+        color:'#0d1e30'
     },
     ripple:{
         simbolo: 'xrp',
         nombre: 'XRP',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/44/thumb/xrp-symbol-white-128.png?1605778731',
-            peque: 'https://assets.coingecko.com/coins/images/44/small/xrp-symbol-white-128.png?1605778731',
-            grande: 'https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731'
-        }
+        color: '#23292f'
     },
     uniswap:{
         simbolo: 'uni',
         nombre: 'Uniswap',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/12504/thumb/uniswap-uni.png?1600306604',
-            peque: 'https://assets.coingecko.com/coins/images/12504/small/uniswap-uni.png?1600306604',
-            grande: 'https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png?1600306604'
-        }
+        color: '#ff007a'
     },
     litecoin:{
         simbolo: 'ltc',
         nombre: 'Litecoin',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/2/thumb/litecoin.png?1547033580',
-            peque: 'https://assets.coingecko.com/coins/images/2/small/litecoin.png?1547033580',
-            grande: 'https://assets.coingecko.com/coins/images/2/large/litecoin.png?1547033580'
-        }
+        color:'#d3d3d3'
     },
     chainlink:{
         simbolo: 'link',
         nombre: 'Chainlink',
-        imagen: {
-            mini: 'https://assets.coingecko.com/coins/images/877/thumb/chainlink-new-logo.png?1547034700',
-            peque: 'https://assets.coingecko.com/coins/images/877/small/chainlink-new-logo.png?1547034700',
-            grande: 'https://assets.coingecko.com/coins/images/877/large/chainlink-new-logo.png?1547034700'
-        }
+        color: '#2a5ada'
     }
 
 };
